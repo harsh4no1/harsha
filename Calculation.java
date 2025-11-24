ba6b2d927e40db5df14545ef9c5f667a2b5d14c8
harsh4no1 <harsh4no1@gmail.com>
2025-11-24 11:14:08 +0100

    Added my assignment file

---
+++ Calculation.java
@@ -0,0 +1,21 @@
+public class Calculation {
+public static void main(String[] args) {
+                Calculation calc = new Calculation();
+calc.calculation();
+    }
+
+public void calculation() {
+    int number1 = 23477777777777;         
+    byte number2 = 123;        
+    float number3 = 123.4f;    
+    char aCharacter = 'E';     
+    String string = "this is a string"; 
+
+    
+    System.out.println("Integer: " + number1);
+    System.out.println("Byte: " + number2);
+    System.out.println("Float: " + number3);
+    System.out.println("Character: " + aCharacter);
+    System.out.println("String: " + string);
+}
+}
